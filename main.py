232fde0275387031860f6a51fc78b9f154ea8988
Octavian Andrei Sava <sava.andrei.octavian@gmail.com>
2022-07-17 13:44:28 +0300

    feat(main): add argparser

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from detector import Detector
 from stream import Stream
+from argparse import ArgumentParser
 
 if __name__ == "__main__":
-
-    detector = Detector(    
-        stream=Stream("http://195.196.36.242/mjpg/video.mjpg"),
-        models=["yolov5s", "yolov5l"],
+    parser = ArgumentParser(description="Process script arguments")
+    parser.add_argument(
+        "--source",
+        type=str,
+        default="http://195.196.36.242/mjpg/video.mjpg",
+        help="Source to the streaming video",
+    )
+    parser.add_argument(
+        "--models",
+        nargs="*",
+        default=["yolov5s", "yolov5l"],
+        help="List with models to be loaded",
+    )
+    args = parser.parse_args()
+    detector = Detector(
+        stream=Stream(args.source),
+        models=args.models,
     )
     detector()
